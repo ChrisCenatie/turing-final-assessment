ea96af3234409e203ff79aa6c23dd1a3469c109f
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-20 18:15:25 -0700

    Updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,4 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :task_lists, only: [:index, :create, :show, :update, :destroy]
-  # root "links#index"
-  #
-  # get '/login', to: 'sessions#new'
-  # post '/login', to: 'sessions#create'
-  # delete '/logout', to: 'sessions#destroy'
-  #
-  # resources :users, only: [:new, :create]
-  # resources :links, only: [:index, :create, :show, :update]
-  #
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :links, only: [:update], defaults: { format: 'json' }
-  #   end
-  # end
 end
