b61016bf4fb706d65c539b5a695412a4d7235e61
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-30 00:58:22 -0700

    Added validation for due and start dates on task model.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,17 @@ class Task < ActiveRecord::Base
   belongs_to :task_list
   validates :title, presence: true
   validates :status, inclusion: { in: %w(complete incomplete) }
+  validate :due_date_cannot_be_in_the_past, :start_date_cannot_be_in_the_past
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "Due date can't be in the past")
+    end
+  end
+
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "Start date can't be in the past")
+    end
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -13,11 +13,33 @@ class TaskTest < ActiveSupport::TestCase
     task_1 = Task.new(title: "Testing", status: "complete")
     task_2 = Task.new(title: "Testing", status: "incomplete")
     task_3 = Task.new(title: "Testing", status: "invalid")
-    
+
     assert task_1.save
     assert task_2.save
     refute task_3.save
     assert_equal task_1.status, "complete"
     assert_equal task_2.status, "incomplete"
+  end
+
+  test "task due date cannot be in the past" do
+    current_date = Date.today
+    valid_task = Task.new(title: "Testing", due_date: current_date)
+    invalid_task = Task.new(title: "Testing", due_date: Date.new(2001, 10, 28))
+
+    assert valid_task.save
+    refute invalid_task.save
+    assert_equal valid_task.due_date, current_date
+    assert_equal invalid_task.errors.messages[:due_date][0], "Due date can't be in the past"
+  end
+
+  test "task start date cannot be in the past" do
+    current_date = Date.today
+    valid_task = Task.new(title: "Testing", start_date: current_date)
+    invalid_task = Task.new(title: "Testing", start_date: Date.new(2001, 10, 28))
+
+    assert valid_task.save
+    refute invalid_task.save
+    assert_equal valid_task.start_date, current_date
+    assert_equal invalid_task.errors.messages[:start_date][0], "Start date can't be in the past"
   end
 end
